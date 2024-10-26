da3b3e4e6e834439fd26ab09e2e027c856743385
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2024-10-25 22:04:59 -0300

    Client id added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -18,7 +18,7 @@ html = """
         <h2>Your ID: <span id="ws-id"></span></h2>
         <form action="" onsubmit="sendMessage(event)">
             <input type="text" class="form-control" id="messageText" autocomplete="off"/>
-            <button class="btn btn-outline-primary mt-2">send</button>
+            <button class="btn btn-outline-primary mt-2">Send</button>
         </form>
         <ul id='messages' class="mt-5">
         </ul>
@@ -26,7 +26,9 @@ html = """
     </div>
     
         <script>
-            var ws = new WebSocket(`ws://localhost:8000/ws`);
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
@@ -49,13 +51,39 @@ def datecheck(date_value):
        date_value = '[Empty]'
     return date_value
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+    
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
 @app.get("/")
 async def rodarhtml():
     return HTMLResponse(html)
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:   
-        date = await websocket.receive_text()
-        await websocket.send_text(f'Message text was {datecheck(date)}')
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            date = await websocket.receive_text()
+            await manager.send_personal_message(f'You wrote: {datecheck(date)}', websocket)
+            await manager.broadcast(f'Client #{client_id} says: {datecheck(date)}')
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f'Client #{client_id} has left the chat')
+
